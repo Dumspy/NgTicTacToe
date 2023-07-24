57e2ad1eb4f29c3e7521c0018f6d810e350b1e6e
Felix Berger <felix.enok.berger@gmail.com>
2023-07-24 11:00:31 +0200

    Cleanup

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -11,16 +11,14 @@ import { animate, style, transition, trigger } from '@angular/animations';
                 ':enter',
                 [
                   style({ opacity: 0 }),
-                  animate('2s ease-out',
-                          style({ opacity: 1 }))
+                  animate('2s ease-out', style({ opacity: 1 }))
                 ]
               ),
               transition(
                 ':leave',
                 [
                   style({ opacity: 1 }),
-                  animate('1s ease-in',
-                          style({ opacity: 0 }))
+                  animate('1s ease-in', style({ opacity: 0 }))
                 ]
               )
             ]
