0d3cb51c53d73a67ffc80afef87e306df50bc6c6
Felix Berger <felix.enok.berger@gmail.com>
2023-07-26 10:23:20 +0200

    Started working on routeing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '',
+    component: BoardComponent,
+    title: 'Test'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
